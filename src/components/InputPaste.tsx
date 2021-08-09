42cac85395c796595029e39c10c80775b0278e5d
Maryam <maryamreis@hotmail.co.uk>
2021-08-09 16:52:58 +0100

    table edit

--- src/components/InputPaste.tsx
+++ src/components/InputPaste.tsx
@@ -3,11 +3,12 @@ import React, {useState} from "react";
 function InputPaste(): JSX.Element {
 
     const [pasteText, setPasteText] = useState("");
+    const [pasteTitle, setPasteTitle] = useState("");
 
     const onSubmitForm = async () => {
         //event.preventDefault();
         try {
-            const body = {pasteText};
+            const body = {pasteText, pasteTitle};
             console.log(body)
             const response = await fetch ("http://localhost:4000/pastes", {
                 method: "POST",
@@ -21,19 +22,33 @@ function InputPaste(): JSX.Element {
             console.log(error.message)
             
         }
-    }
+    };
 
     return (
         <div> 
-            <h1>Input Paste</h1>
-            <form>
-                <input 
-                value = {pasteText}
-                onChange = {event => setPasteText(event.target.value)}  
-                />
-                <button
-                onClick = {onSubmitForm}>Save</button>
-            </form>
+            <div>
+                <h1 className = "text-center mt-5">Input Paste</h1>
+                <form className = "d-flex mt-2">
+                    <input 
+                        type = "text"
+                        className = "form-control"
+                        value = {pasteText}
+                        placeholder = "paste text here"
+                        onChange = {event => setPasteText(event.target.value)}  
+                    />
+                    <input 
+                        type = "text"
+                        className = "form-control col-md-3"
+                        value = {pasteTitle}
+                        placeholder = "optional - title of text here"
+                        onChange = {event => setPasteTitle(event.target.value)}
+                            
+                    />
+                    <button
+                    onClick = {onSubmitForm}
+                    className = "btn btn-success">Save</button>
+                </form>
+            </div>
 
         </div>
     )
--- src/components/ListPaste.tsx
+++ src/components/ListPaste.tsx
@@ -1,8 +1,82 @@
-import React from "react";
+import { getPackedSettings } from "http2";
+import React, {useState, useEffect} from "react";
 
 function ListPaste(): JSX.Element {
+
+    const [pasteList, setPasteList] = useState<any[]>([]);
+
+    // delete function
+    const deletePaste = async (id: number) => {
+        try {
+            const deletePaste = await fetch(`http://localhost:4000/pastes/${id}`, {
+                method: "DELETE"
+            });
+            
+            setPasteList(pasteList.filter(paste => paste.id !== id));
+
+            console.log(deletePaste);
+
+            
+        } catch (error) {
+            console.error(error.message)
+            
+        }
+    }
+
+    // get function
+    const getPastes = async () => {
+        try {
+
+            const response = await fetch("http://localhost:4000/pastes");
+            const jsonData = await response.json();
+            console.log(jsonData);
+
+            setPasteList(jsonData);
+            
+        } catch (error) {
+            console.error(error.message)
+            
+        }
+    };
+
+    useEffect(() => {
+        getPastes();
+    }, []);
+
+    console.log(pasteList);
+
     return (
-        <h1>List of Pastes</h1>
+        <div>
+            <table className = "table table-striped mt-5">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Text</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    {pasteList.map(paste => (
+                        <tr key = {paste.id}>
+                            <td>{paste.paste_title}</td>
+                            <td>{paste.paste_text}</td>
+                            <td>Edit</td>
+                            <td>
+                                <button 
+                                    className = "btn btn-danger"
+                                    onClick = {() => deletePaste(paste.id)}
+                                >
+                                        Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
